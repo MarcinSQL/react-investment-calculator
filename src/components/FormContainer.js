5547d6d5eafaa92497c97390329bc0a62b01d51c
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-13 18:07:27 +0200

    Added useState to form inputs

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,14 +1,42 @@
-const FormContainer = () => {
-  const calculateHandler = (userInput) => {
+import React, { useState } from "react";
+
+const FormContainer = (props) => {
+  const [enteredCurrentSavings, setEnteredCurrentSavings] = useState();
+  const [enteredYearlyContribution, setEnteredYearlyContribution] = useState();
+  const [enteredExpectedReturn, setEnteredExpectedReturn] = useState();
+  const [enteredDuration, setEnteredDuration] = useState();
+
+  const currentSavingsChangeHandler = (e) => {
+    setEnteredCurrentSavings(e.target.value);
+  };
+  const yearlyContributionChangeHandler = (e) => {
+    setEnteredYearlyContribution(e.target.value);
+  };
+  const expectedReturnChangeHandler = (e) => {
+    setEnteredExpectedReturn(e.target.value);
+  };
+  const durationChangeHandler = (e) => {
+    setEnteredDuration(e.target.value);
+  };
+
+  const calculateHandler = (event) => {
+    event.preventDefault();
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
 
+    const userInputs = {
+      currentSavings: +enteredCurrentSavings,
+      yearlyContribution: +enteredYearlyContribution,
+      expectedReturn: +enteredExpectedReturn / 100,
+      duration: +enteredDuration,
+    };
+
     const yearlyData = []; // per-year results
 
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
+    // let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
+    // const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    // const expectedReturn = +userInput["expected-return"] / 100;
+    // const duration = +userInput["duration"];
 
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
@@ -27,15 +55,25 @@ const FormContainer = () => {
   };
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={calculateHandler}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            value={enteredCurrentSavings}
+            onChange={currentSavingsChangeHandler}
+            id="current-savings"
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            value={enteredYearlyContribution}
+            onChange={yearlyContributionChangeHandler}
+            id="yearly-contribution"
+          />
         </p>
       </div>
       <div className="input-group">
@@ -43,11 +81,21 @@ const FormContainer = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            value={enteredExpectedReturn}
+            onChange={expectedReturnChangeHandler}
+            id="expected-return"
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            value={enteredDuration}
+            onChange={durationChangeHandler}
+            id="duration"
+          />
         </p>
       </div>
       <p className="actions">
